033c8532f17d6d7bbd08d262407d7f4c25a0692d
Ywoosang <opellong13@gmail.com>
2021-09-24 01:26:56 +0900

    Add configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv';
+dotenv.config(); 
+
 export default {
    test: {
         host: "localhost",
@@ -6,6 +9,7 @@ export default {
         password: "db11",
         database : "ClothingTestDB",
         connectionLimit: 5000,
+        dateStrings: "date"
     }, 
     development : {
         host: "localhost",
@@ -14,9 +18,17 @@ export default {
         password: "db11",
         database: "ClothingDB",
         connectionLimit: 5000,
+        dateStrings: "date",
     },
     production : {
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        port: process.env.MYSQL_PORT,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        connectionLimit: 5000,
+        dateStrings: "date",
     }
-}
+} as any;
 
  
