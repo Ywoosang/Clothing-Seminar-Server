1c0892bbb2d47d17108b6232ddfea4b433c5b767
Ywoosang <opellong13@gmail.com>
2021-10-26 23:58:49 +0900

    Add AdminDao

--- src/models/admin.model.ts
+++ src/models/admin.model.ts
@@ -1,28 +1,32 @@
 import connection from '../db/connection';
 
-export async function getAllUsers(){
-    const [users] = await connection.promise().query(`
-    SELECT username,userid,authority 
-    FROM User 
-    WHERE authority <> 'ADMINISTRATOR';
-    `); 
-    return users;
+class AdminDao{
+    public static async getAllUsers(){
+        const [users] = await connection.promise().query(`
+        SELECT username,userid,authority 
+        FROM User 
+        WHERE authority <> 'ADMINISTRATOR';
+        `); 
+        return users;
+    }; 
+
+    public static async getUserAuthority(userId:string){
+        const [rows] = await connection.promise().query(`
+        SELECT authority 
+        FROM User 
+        WHERE userid = '${userId}'
+        `);
+        return rows[0].authority; 
+    };
+
+    public static async changeUserAuthority(authority: string, userId:string){
+        await connection.promise().query(`
+        UPDATE User 
+        SET authority = '${authority}' 
+        WHERE userid = '${userId}';
+        `);
+    }; 
+
 }
 
-export async function getUserAuthority(userId:string){
-    const [rows] = await connection.promise().query(`
-    SELECT authority 
-    FROM User 
-    WHERE userid = '${userId}'
-    `);
-    return rows[0].authority; 
-}
-
-export async function changeUserAuthority(authority: string, userId:string){
-    await connection.promise().query(`
-    UPDATE User 
-    SET authority = '${authority}' 
-    WHERE userid = '${userId}';
-    `);
-}
-
+export default AdminDao;
